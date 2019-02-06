aeba1c96ce1c3affda13b1e95f6eda5d08e73c3c
Brad Cowie <brad@wand.net.nz>
2019-02-07 11:30:10 +1300

    Add apt-get upgrade back.

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -9,6 +9,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
     apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git
+apt-get upgrade -y
 
 dpkg-parsechangelog -S version | grep -q ${CI_COMMIT_REF_NAME} || debchange --newversion ${CI_COMMIT_REF_NAME} -b "New upstream release"
 mk-build-deps -i -r -t 'apt-get -f -y --force-yes'
