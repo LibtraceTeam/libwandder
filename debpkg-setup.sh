add0f2974cfcfe2f570f271efe27a121605aa3d4
Shane Alcock <salcock@waikato.ac.nz>
2020-06-09 15:07:21 +1200

    Update CI to use github actions instead of gitlab
    Added ability to upload packages to cloudsmith as well as bintray.

---
+++ debpkg-setup.sh
@@ -0,0 +1,13 @@
+et -x -e -o pipefail
+
+export DEBEMAIL='packaging@wand.net.nz'
+export DEBFULLNAME='WAND Packaging'
+export DEBIAN_FRONTEND=noninteractive
+
+export SOURCENAME=`echo ${GITHUB_REF##*/} | cut -d '-' -f 1`
+
+apt-get update
+apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
+    apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git
+
+apt-get upgrade -y
