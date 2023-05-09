e70bcb5089baf26041ddf5034d0788bea4e41a52
Shane Alcock <shane@alcock.co.nz>
2023-05-10 10:55:52 +1200

    Update bootstrap.sh to work on Mac OS X

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #! /bin/sh
 
 set -x
-aclocal-1.9 || aclocal 
-libtoolize --force --copy
+aclocal
+libtoolize --force --copy || glibtoolize --force --copy
 autoheader
-automake-1.9 --add-missing --copy --foreign ||
-        automake --add-missing --copy --foreign
-	
+automake --add-missing --copy --foreign
 autoconf
