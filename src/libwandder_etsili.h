6ab614742db5bf14acf695f2cbb203074787e7d7
Shane Alcock <shane@alcock.co.nz>
2025-02-17 11:33:22 +1300

    Update copyright for all source files

--- src/libwandder_etsili.h
+++ src/libwandder_etsili.h
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright (c) 2017-2019 The University of Waikato, Hamilton, New Zealand.
+ * Copyright (c) 2024, 2025 SearchLight Ltd, New Zealand.
  * All rights reserved.
  *
  * This file is part of libwandder.
  *
- * This code has been developed by the University of Waikato WAND
+ * Libwandder was originally developed by the University of Waikato WAND
  * research group. For further information please see http://www.wand.net.nz/
  *
  * libwandder is free software; you can redistribute it and/or modify
